c82e7676e73967d3844a3bdd0ca7e39c3a370bed
santirampoldi <santirampoldi12@gmail.com>
2018-11-09 17:23:38 -0300

    Añadido elementos y animacion 3D a la pagina

--- js/tudaiGames.js
+++ js/tudaiGames.js
@@ -1,5 +1,27 @@
 document.addEventListener("DOMContentLoaded", function(event) {
 
-   
 
 });
+
+
+function getDatos() {
+  fetch('https://jsonplaceholder.typicode.com/comments')
+  .then(response => response.json())
+  .then(function(json) {
+    mostrarDatos(json);
+  });
+}
+
+function mostrarDatos(json) {
+  let html ='';
+  for (var i = 0; i < 10; i++) {
+    console.log(json[i]);
+    html += "<tr id=" + json[i].id + ">"
+    html += "<td>" + json[i].email
+    html += "</td><td>" + json[i].name
+    html += "</td><td>" + json[i].body
+    html += "</td></tr>";
+  }
+  let jqHTML = $(html);
+  $(".comments").html('').append(jqHTML);
+}
