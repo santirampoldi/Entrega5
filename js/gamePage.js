b64f0349f93eab778c18ab75ab657b16bbf83fcb
ameliendrez <ameliendrez@gmail.com>
2018-11-21 17:19:26 -0300

    agregado comentarios en la pagina de juegos

---
+++ js/gamePage.js
@@ -0,0 +1,33 @@
+getDatos();
+
+function getDatos() {
+    fetch('https://jsonplaceholder.typicode.com/comments')
+    .then(response => response.json())
+    .then(function (json) {
+      mostrarDatos(json);
+    });  
+}
+
+function mostrarDatos(json) {
+  let html = '';
+  for (var i = 0; i < 5; i++) {
+
+    html += '<div class="container single-comment">';
+    html += '<div class="row"><div class="col-12"></div></div>';
+    html += '<div class="row"><div class="col-1"><div class="thumbnail">';
+    html += '<img class="img-fluid user-photo" src="https://ssl.gstatic.com/accounts/ui/avatar_2x.png"></div></div>';
+    html += '<div class="col-11"><div class="card"><div class="card-header">';
+    html += '<strong>'+json[i].email+'</strong> <span class="text-muted">commented 5 days ago</span></div></div>'
+    html += '<div class="card-body">' +json[i].body+ '</div></div></div></div></div>';
+
+
+    // html += "<tr id=" + json[i].id + ">"
+    // html += "<td>" + json[i].email
+    // html += "</td><td>" + json[i].name
+    // html += "</td><td>" + json[i].body
+    // html += "</td></tr>";
+  }
+  let jqHTML = $(html);
+  $(".comments").html('').append(jqHTML);
+}
+
--- js/tudaiGames.js
+++ js/tudaiGames.js
@@ -47,25 +47,3 @@ function updatePriceLabels() {
   } 
 } 
 
-
-function getDatos() {
-  fetch('https://jsonplaceholder.typicode.com/comments')
-    .then(response => response.json())
-    .then(function (json) {
-      mostrarDatos(json);
-    });
-}
-
-function mostrarDatos(json) {
-  let html = '';
-  for (var i = 0; i < 10; i++) {
-    console.log(json[i]);
-    html += "<tr id=" + json[i].id + ">"
-    html += "<td>" + json[i].email
-    html += "</td><td>" + json[i].name
-    html += "</td><td>" + json[i].body
-    html += "</td></tr>";
-  }
-  let jqHTML = $(html);
-  $(".comments").html('').append(jqHTML);
-}
